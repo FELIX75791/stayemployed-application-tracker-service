e8a5f68be9723fab413c971a97b786c9c25d2b5e
KenD2002 <dengken1@g.ucla.edu>
2024-12-09 23:29:53 -0500

    FIX:minor changes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
+import os
 from fastapi import FastAPI
 from app.db import Base, sync_engine
 from app.routes import job_application_routes
 from app.middleware.logging_middleware import BeforeAfterLoggingMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-
 app = FastAPI()
 
 Base.metadata.create_all(bind=sync_engine)
@@ -13,15 +13,14 @@ app.add_middleware(BeforeAfterLoggingMiddleware)
 
 # Configure CORS
 origins = [
-    "http://localhost",
-    "http://localhost:3000"
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Allow specific IP (127.0.0.1) with all ports
+    allow_origins=origins,
     allow_credentials=True,             # Allow credentials (e.g., cookies, authorization headers)
-    allow_methods=["POST"],                # Allow all HTTP methods (GET, POST, etc.)
+    allow_methods=["*"],                # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],                # Allow all HTTP headers
 )
 
@@ -33,4 +32,5 @@ async def root():
     return {"message": "Job Application Tracker Microservice"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
